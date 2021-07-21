6f99479dff7cf69b21320c78d46faaab6b999506
Hosuu <48166166+Hosuu@users.noreply.github.com>
2021-07-21 17:09:55 +0200

    Init

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const GAME_WIDTH = 320
+export const GAME_HEIGHT = 180
+
+export const PADDLE_WIDTH = 5
+export const PADDLE_HEIGHT = 30
+export const PADDLE_SPEED = 0.1
+export const PADDLE_X_OFFSET = 10
+
+export const BALL_SIZE = 5
+export const BALL_SPEED = 0.15
+export const BALL_SPEED_MULTIPLER = 1.1
